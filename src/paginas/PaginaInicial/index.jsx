d5b4e838a20a982e6e4f114ee709e8dcdc1efc00
Romildo Roberto <romildoroberto123@gmail.com>
2023-06-11 17:48:06 -0300

    First commit

---
+++ src/paginas/PaginaInicial/index.jsx
@@ -0,0 +1,11 @@
+import Cabecalho from "../../componentes/Cabecalho";
+import Menu from "../../componentes/Menu";
+
+export default function PaginaInicial() {
+    return (
+        <>
+            <Cabecalho />
+            <Menu />
+        </>
+    )
+}
